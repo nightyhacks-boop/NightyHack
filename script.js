05a98da2b3de9ab9fd870527517cd10b0725b560
nightyhacks-boop <nightyhacks@gmail.com>
2026-03-17 00:44:51 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("downloadBtn");
-
-btn?.addEventListener("click", () => {
-  document.getElementById("downloads").scrollIntoView({behavior:"smooth"});
-});
-
-function fakeDownload(){
-  alert("NightyHack Loader download starting...");
-}
